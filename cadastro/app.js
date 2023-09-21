fd87d284004e33c513bdbdcb36deb0afe9cd05d7
vitorgabrieldev <vitorgabrieldeoliveiradev@gmail.com>
2023-09-21 14:47:47 -0300

    Remove coments js

--- cadastro/app.js
+++ cadastro/app.js
@@ -1,49 +1,55 @@
-'use strict'; //Modo "Restrito"
-//Consumindo API de CEP, do ViaCep
-// https://viacep.com.br/
+'use strict';
 
-//Limpa o Form (do CEP pra baixo)...
-const limparFormulario = (endereco) =>{
+const cleanForm = (endereco) =>{
     document.getElementById('rua').value = '';
     document.getElementById('bairro').value = '';
     document.getElementById('cidade').value = '';
     document.getElementById('estado').value = '';
-}
+};
 
-//Preenche os campos relacionados ao CEP...
-const preencherForumulario = (endereco) =>{
+const fillOutForm= (endereco) =>{
     document.getElementById('rua').value = endereco.logradouro;
     document.getElementById('bairro').value = endereco.bairro;
     document.getElementById('cidade').value = endereco.localidade;
     document.getElementById('estado').value = endereco.uf;
-}
+};
 
-//Verifica se o CEP é válido...
-const eNumero = (numero) => /^[0-9]+$/.test(numero); //Expressão Regular
-// É possível testar e entender a RegEx em https://www.regexpal.com/
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
 const cepValido = (cep) => cep.length == 8 && eNumero(cep);
 
-//Consumindo API... 2- passo
-const pesquisarCep = async() => {
-    limparFormulario();
+const searchCEP = async() => {
+    cleanForm();
     const url = `https://viacep.com.br/ws/${cep.value}/json/`;
     
     if(cepValido(cep.value)){
-        const dados = await fetch(url); //await = esperar
-        const addres = await dados.json(); // fetch = promessa
+        const dados = await fetch(url);
+        const addres = await dados.json();
         
-        // hasOwnProperty  retorna um booleano indicando se o objeto possui a propriedade especificada como uma propriedade definida no próprio objeto em questão
         if(addres.hasOwnProperty('erro')){ 
-            // document.getElementById('rua').value = 'CEP não encontrado!';
             alert('CEP não encontrado!');
         }else {
-            preencherForumulario(addres);
-        }
+            fillOutForm(addres);
+        };
     }else{
-        // document.getElementById('rua').value = 'CEP incorreto!'; 
         alert('CEP incorreto!');
-    } 
-}
+    };
+};
 
-//Adicionando um evento DOM, no input CEP... 1- passo
-document.getElementById('cep').addEventListener('focusout', pesquisarCep);
+document.getElementById('cep').addEventListener('focusout', searchCEP);
+
+const registerAdress = () => {
+
+    if (document.getElementById("rua").value) {
+        Swal.fire(
+            'Cadastro realizado com sucesso!',
+            'Você será redirecionado em alguns instantes...',
+            'success'
+        );   
+    } else {
+        Swal.fire(
+            'Erro de cadastro!',
+            'Preenche todos os campos',
+            'error'
+        );
+    };
+};
